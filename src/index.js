bee40117cfee34e03fd8b81a305ad56c942bb6e2
uzuzer567 <49733302+uzuzer567@users.noreply.github.com>
2021-02-10 20:07:38 +0300

    feat: implementation of function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+	if(n < 0) {
+		n * (-1);
+	}
+	while(n % 10 == 0) {
+		n = n / 10;
+	}
+	var answer = n.toString().split('').reverse().join('');
+	return parseInt(answer);
 }
